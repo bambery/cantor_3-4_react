84fa7cca8b839013a92dd4415404a4f87d3f0ee0
bambery <lwszolek@gmail.com>
2022-12-12 13:06:53 -0600

    test working using template literals, but I am going to move off of them

--- src/models/fraction.js
+++ src/models/fraction.js
@@ -1,4 +1,7 @@
-import { lcm } from '../shared/utils'
+import { lcm, type } from '../shared/utils'
+
+window.lcm = lcm
+window.type = type
 
 class Fraction {
     #numerator = 1
@@ -21,6 +24,9 @@ class Fraction {
 
     // given 2 fractions, convert them to have a common denominator and return them in a 2 item Array
     static commonDen(fracA, fracB){
+        if (type(fracA) !== 'Fraction' || type(fracB) !== 'Fraction'){
+            throw new Error('Must pass two fractions as separate arguments to Fraction.commonDen')
+        }
         const common = lcm([fracA.den, fracB.den])
         let numA = (fracA.num * (common/fracA.den))
         let numB = (fracB.num * (common/fracB.den))
@@ -52,40 +58,59 @@ class Fraction {
 
         return new Fraction(this.#numerator/x, this.#denominator/x)
     }
-
+/*
     reduce_() {
         let temp = this.reduce()
         this.#numerator = temp.num
         this.#denominator = temp.den
     }
+    */
 
     // subtract the passed in fraction from and return a new Fraction
     subtract(rhs) {
+        if (type(rhs) !== 'Fraction'){
+            throw new Error('Must pass only Fraction argument')
+        }
         let [ leftHS, rightHS ] = Fraction.commonDen(this, rhs)
         let numerator = leftHS.num - rightHS.num
+        if(numerator < 0){
+            throw new Error(`ERROR: Difference of ${this.str} and ${rhs.str} would be less than 0.`)
+        }
         return new Fraction(numerator, leftHS.den)
     }
-
+/*
     subtract_(rhs) {
+        if (type(rhs) !== 'Fraction'){
+            throw new Error('Must pass only Fraction argument')
+        }
         let temp = this.subtract(rhs)
         this.#numerator = temp.num
         this.#denominator = temp.den
         return this
     }
+    */
 
     // add the passed in fraction and return a new Fraction
     add(rhs) {
+        if (type(rhs) !== 'Fraction'){
+            throw new Error('Must pass only Fraction argument')
+        }
+
         let [ leftHS, rightHS ] = Fraction.commonDen(this, rhs)
         let numerator = leftHS.num + rightHS.num
+        if (numerator > leftHS.den){
+            throw new Error(`ERROR: Sum of ${this.str} and ${rhs.str} would be greater than 1.`)
+        }
         return new Fraction(numerator, leftHS.den)
     }
-
+/*
     add_(rhs) {
         let temp = this.add(rhs)
         this.#numerator = temp.num
         this.#denominator = temp.den
         return this
     }
+    */
 }
 
 export default Fraction
--- src/test/fraction.test.js
+++ src/test/fraction.test.js
@@ -27,40 +27,140 @@ describe('Fractions', function() {
 
     })
 
-    describe('modifying Fractions', function() {
-        it('cannot directly modify numerator or denominator', function() {
-            let frac = new Fraction(3, 4)
-            expect( () => {frac.num = 1}).toThrow()
-            expect( () => {frac.den= 1}).toThrow()
-        })
+    it('cannot directly modify numerator or denominator', function() {
+        let frac = new Fraction(3, 4)
+        expect( () => frac.num = 1).toThrow()
+        expect( () => frac.den= 1).toThrow()
+    })
 
-        describe('addition via "add"', function() {
-            //weirdly, one does NOT use a before block here with Mocha
-            let frac1 = new Fraction(2, 5)
-            let frac2 = new Fraction(1, 5)
-
-            let sum1 = frac1.add(frac2)
-            let sum2 = frac2.add(frac1)
+    describe('Fraction arithmetic', function() {
+        describe('addition', function() {
+            let frac_2_5 = new Fraction(2, 5)
+            let frac_1_5 = new Fraction(1, 5)
+            let frac_1_3 = new Fraction(1, 3)
+            let frac_1_6 = new Fraction(1, 6)
+            let frac_4_5 = new Fraction(4, 5)
 
             it('does not modify the original fractions', function() {
-                expect(frac1.num).toEqual(2)
-                expect(frac1.den).toEqual(5)
+                let sum = frac_2_5.add(frac_1_5)
+                expect(frac_2_5.num).toEqual(2)
+                expect(frac_2_5.den).toEqual(5)
 
-                expect(frac2.num).toEqual(1)
-                expect(frac2.den).toEqual(5)
+                expect(frac_1_5.num).toEqual(1)
+                expect(frac_1_5.den).toEqual(5)
             })
 
             it('can add two fractions with the same denominator', function() {
-                expect(sum1.num).toEqual(3)
-                expect(sum1.den).toEqual(5)
-
+                let sum = frac_2_5.add(frac_1_5)
+                expect(sum.num).toEqual(3)
+                expect(sum.den).toEqual(5)
             })
 
             it('returns the same result regardless of order of operands', function(){
-                expect(sum1.num).toEqual(sum2.num)
-                expect(sum1.den).toEqual(sum2.den)
+                let sum = frac_2_5.add(frac_1_5)
+                let sum2 = frac_1_5.add(frac_2_5)
+                expect(sum.num).toEqual(sum2.num)
+                expect(sum.den).toEqual(sum2.den)
             })
 
+            it('can add two fractions of different denominators', function(){
+                let sum = frac_1_5.add(frac_1_3)
+
+                expect(sum.num).toEqual(8)
+                expect(sum.den).toEqual(15)
+            })
+
+            it('does not reduce the sum', function(){
+                let sum = frac_1_6.add(frac_1_3)
+
+                expect(sum.num).toEqual(3)
+                expect(sum.den).toEqual(6)
+            })
+
+            it('fails if the sum would be greater than 1', function(){
+                expect( ()=> frac_4_5.add(frac_2_5) ).toThrow()
+            })
+
+            it('succeeds if the sum would be 1', function() {
+                let sum = frac_4_5.add(frac_1_5)
+                expect(sum.num).toEqual(5)
+                expect(sum.den).toEqual(5)
+            })
+
+        })
+
+        describe('subtraction', function(){
+            let frac_5_7 = new Fraction(5, 7)
+            let frac_3_7 = new Fraction(3, 7)
+            let frac_2_5 = new Fraction(2, 5)
+
+            it('does not modify the original fractions', function(){
+                let diff = frac_5_7.subtract(frac_2_5)
+
+                expect(frac_5_7.num).toEqual(5)
+                expect(frac_5_7.den).toEqual(7)
+
+                expect(frac_2_5.num).toEqual(2)
+                expect(frac_2_5.den).toEqual(5)
+            })
+
+            it('can subtract two fractions with the same denominator', function(){
+                let diff = frac_5_7.subtract(frac_3_7)
+
+                expect(diff.num).toEqual(2)
+                expect(diff.den).toEqual(7)
+            })
+
+            it('can subtract two fractions of different denominators', function(){
+                let diff = frac_5_7.subtract(frac_2_5)
+
+                expect(diff.num).toEqual(11)
+                expect(diff.den).toEqual(35)
+
+            })
+
+            it('fails if the result would be negative', function(){
+                expect( () => frac_2_5.subtract(frac_5_4) ).toThrow()
+            })
+
+            it('succeeds if the result would be zero', function(){
+                let diff = frac_2_5.subtract(frac_2_5)
+
+                expect(diff.num).toEqual(0)
+                expect(diff.den).toEqual(5)
+            })
+        })
+
+        describe('reducing a fraction', function(){
+            it.todo('does not reduce a fraction with zero in the numerator')
+
+            it.todo('does not reduce a fraction that has the same numerator and denominator')
+
+            describe('does not reduce fractions that cannot be reduced', function(){
+                it.each`
+                    orig
+                    ${new Fraction(1,4)}
+                    ${new Fraction(13, 94)}
+                    ${new Fraction(27, 35)}
+                    ${new Fraction(3, 86)}
+                    `('do not reduce $orig.str', ({orig}) => {
+                        expect(orig.reduce().num).toEqual(orig.num)
+                        expect(orig.reduce().den).toEqual(orig.den)
+                    })
+            })
+
+            describe('properly reduces fractions', function(){
+                it.each`
+                    orig                    | result
+                    ${new Fraction(2,4)}    | ${new Fraction(1,2)}
+                    ${new Fraction(60, 94)} | ${new Fraction(30, 47)}
+                    ${new Fraction(60, 92)} | ${new Fraction(15, 23)}
+                    ${new Fraction(56, 86)} | ${new Fraction(28, 43)}
+                    `('reduce $orig.str to $result.str', ({orig, result}) => {
+                        expect(orig.reduce().num).toEqual(result.num)
+                        expect(orig.reduce().den).toEqual(result.den)
+                })
+            })
         })
     })
 })
