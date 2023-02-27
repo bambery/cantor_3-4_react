6dcf42eb2ab5f0e9ca01901229812c7d3164bb67
bambery <lwszolek@gmail.com>
2023-02-27 16:00:14 -0600

    added equation for Cantor, diagrams, explanation of set construction

--- src/components/FracDisplay.js
+++ src/components/FracDisplay.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Fraction from '../models/fraction'
+import styles from './FracDisplay.module.css'
 
 const FracDisplay = ({ frac }) => {
     return(
-        <span className='frac-display'>
+        <span className={styles.fracDisplay}>
             <span>{frac.num}</span>
-            <span className='frac-line'>
+            <span className={styles.fracLine}>
             </span>
             <span>{frac.den}</span>
         </span>
@@ -14,14 +15,7 @@ const FracDisplay = ({ frac }) => {
 }
 
 FracDisplay.propTypes = {
-    frac: PropTypes.instanceOf(Fraction)
+    frac: PropTypes.object.isRequired
 }
 
 export default FracDisplay
-/*
-<div>a <span style="display:inline-flex;flex-direction:column;vertical-align:middle;">
-<span>1</span>
-<span style="border-top:1px solid">2</span>
-</span> b
-</div>
-*/
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { AiFillCaretDown, AiFillCaretRight } from 'react-icons/ai'
 import styles from './Introduction.module.css'
 import HowToCantorStep from './HowToCantorStep'
+import FracDisplay from './FracDisplay'
 
 const Introduction = () => {
 
@@ -42,6 +43,51 @@ const Introduction = () => {
         )
     }
 
+    const parensSec = () => {
+        const den = <span>1 - {frac(2,3, 'evenSmallerFrac')}</span>
+        return(
+            <span className={styles.bigParens}>
+                <span className={styles.parens}>(</span>
+                {frac(1, den)}
+                <span className={styles.parens}>)</span>
+            </span>
+        )
+    }
+
+    const geometricSum = (start,end) => {
+        const top = <span>2<sup>n</sup></span>
+        const bottom = <span>3<sup>n+1</sup></span>
+        return(
+            <div className={styles.eq}>
+                <div className={styles.sum}>
+                    <div className={styles.end}>
+                        {'\u221e'}
+                    </div>
+                    <div className={styles.sigma}>
+                        {'\u2211'}
+                    </div>
+                    <div className={styles.start}>
+                        n=0
+                    </div>
+                </div>
+                <div className={styles.equation}>
+                    <span>{frac(top, bottom)} = {frac(1,3)} + {frac(2,9)} + {frac(4, 27)} + {frac(8, 81)} + {'\u22ef'} + = {frac(1,3)}</span> {parensSec()} <span>= 1</span>
+                </div>
+            </div>
+        )
+    }
+
+    const frac = (num, den, optionalClass='smallerFrac') => {
+        const newFrac = {
+            'num': num,
+            'den': den
+        }
+
+        return(
+            <span className={styles[optionalClass]}><FracDisplay frac={newFrac}/></span>
+        )
+    }
+
     const cantorDescription = () => {
         return(
             <div className={styles.textDescription}>
@@ -52,10 +98,18 @@ const Introduction = () => {
                     </div>
                     <div className={styles.twoCol}>
                         { stepDetails.map( (stepInfo, idx) =>
-                            <React.Fragment key={stepInfo['numeral']}>
-                                <HowToCantorStep stepInfo={stepInfo} stepNum={idx} />
-                            </React.Fragment>
+                        <React.Fragment key={stepInfo['numeral']}>
+                            <HowToCantorStep stepInfo={stepInfo} stepNum={idx} />
+                        </React.Fragment>
                         ) }
+                    </div>
+                    <div className={styles.textDescription}>
+                        <p><span className={styles.introEmphasis}>How many points are in the Cantor Set?</span>{`Because the endpoints of each interval are never candidates for removal (each iteration must remove a "middle" interval), the set of all points remaining after each iteration will never be empty, making the Cantor Set an `}<a href="https://en.wikipedia.org/wiki/Infinite_set">infinite set</a>{`.`}</p>
+                        <p><span className={styles.introEmphasis}>What is the length of the Cantor Set?</span> The original length of the interval is 1. In the first iteration, we remove an interval of length {frac(1,3)}, leaving 1 - {frac(1,3)} = {frac(2,3)}. In the second iteration, we remove 2 intervals of length {frac(1,9)}, leaving {frac(2,3)} - {frac(1,9)}*2 = {frac(4,9)}. A third iteration would remove 4 intervals of length {frac(1, 27)}, leaving length {frac(4,9)} - {frac(4, 27)} = {frac(8,27)}.</p>
+                        <p>The total length of a Cantor Set can be computed by subtracting the total length of all removed intervals:</p>
+                        <div className={styles.geometricEq}>
+                            {geometricSum()}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -66,13 +120,13 @@ const Introduction = () => {
         <div className={styles.centeringDiv}>
             <div className={styles.introduction} onClick={toggleDetails}>
                 <div className={styles.whatIsCantorTitle}>
-                {displayToggle()}
-                What is a Cantor Set?
+                    {displayToggle()}
+                    What is a Cantor Set?
                 </div>
                 {showCantorInfo && cantorDescription()}
             </div>
             <div className={styles.introduction}>
-                This is an introduction
+                This tool will construct both symmetric and asymmetric Cantor-like sets based on your custom inputs. The numberline in black below shows an example of the segmentation to be performed on each iteration. Results can be downloaded in CSV format.
             </div>
         </div>
     )
